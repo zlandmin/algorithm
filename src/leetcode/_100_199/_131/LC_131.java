76c17704fef9ea7ae1383194718f03e45ea5fa41
lei zhang <zlandmin@gmail.com>
2020-06-23 13:25:05 -0700

    June 23

---
+++ src/leetcode/_100_199/_131/LC_131.java
@@ -0,0 +1,21 @@
+package leetcode._100_199._131;
+
+import java.util.List;
+
+/*
+131. Palindrome Partitioning
+Given a string s, partition s such that every substring of the partition is a palindrome.
+
+Return all possible palindrome partitioning of s.
+
+Example:
+
+Input: "aab"
+Output:
+[
+  ["aa","b"],
+  ["a","a","b"]
+]
+ */
+public class LC_131 {
+}
