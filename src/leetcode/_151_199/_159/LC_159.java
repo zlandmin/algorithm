a6801d4e19554bc884c39af651218ec16033a1fd
lei zhang <zlandmin@gmail.com>
2020-07-05 23:12:03 -0700

    July 5

--- src/leetcode/_151_199/_159/LC_159.java
+++ src/leetcode/_151_199/_159/LC_159.java
@@ -1,7 +1,49 @@
 package leetcode._151_199._159;
 
-/*
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 159. Longest Substring with At Most Two Distinct Characters
+ * Given a string s , find the length of the longest substring t  that contains at most 2 distinct characters.
+ *
+ * Example 1:
+ * Input: "eceba"
+ * Output: 3
+ * Explanation: t is "ece" which its length is 3.
+ *
+ * Example 2:
+ * Input: "ccaabbb"
+ * Output: 5
+ * Explanation: t is "aabbb" which its length is 5.
  */
 public class LC_159 {
+    public int lengthOfLongestSubstringTwoDistinct(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
 
+        Map<Character, Integer> map = new HashMap<>();
+        int start = 0;
+        int index = 0;
+        int res = 0;
+
+        while (index < s.length()) {
+            map.put(s.charAt(index), index);
+            index++;
+
+            if (map.size() > 2) {
+                int left = s.length();
+                for (int num : map.values()) {
+                    left = Math.min(left, num);
+                }
+                map.remove(s.charAt(left));
+                start = left + 1;
+            }
+
+            res = Math.max(res, index - start);
+        }
+
+        return res;
+    }
 }
--- src/leetcode/_151_199/_163/LC_163.java
+++ src/leetcode/_151_199/_163/LC_163.java
@@ -1,7 +1,39 @@
 package leetcode._151_199._163;
 
-/*
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 163. Missing Ranges
+ *
+ * Given a sorted integer array where the range of elements are in the inclusive range [lower, upper], return its missing ranges.
+ * For example, given [0, 1, 3, 50, 75], lower = 0 and upper = 99, return ["2", "4->49", "51->74", "76->99"].
  */
 public class LC_163 {
+    public List<String> findMissingRanges(int[] nums, int lower, int upper) {
+        List<String> res = new ArrayList<>();
+        long alower = (long)lower;
+        long aupper = (long)upper;
 
+        for (int num : nums) {
+            if (alower == num) {
+                alower++;
+            } else if (alower < num) {
+                if (alower + 1 == num) {
+                    res.add(String.valueOf(alower));
+                } else {
+                    res.add(alower + "->" + (num - 1));
+                }
+                alower = (long)num + 1;
+            }
+        }
+
+        if (alower == upper) {
+            res.add(String.valueOf(upper));
+        } else if (alower < upper){
+            res.add(alower + "->" + upper);
+        }
+
+        return res;
+    }
 }
