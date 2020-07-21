95e9bc76780fd680d2a7dd38403b8b77e4ad838c
lei zhang <zlandmin@gmail.com>
2020-07-20 22:04:04 -0700

    July 20

--- src/leetcode/_200_250/_203/LC_203.java
+++ src/leetcode/_200_250/_203/LC_203.java
@@ -1,4 +1,27 @@
 package leetcode._200_250._203;
 
+import leetcode.common.ListNode;
+
+/**
+ * 203. Remove Linked List Elements
+ * Remove all elements from a linked list of integers that have value val.
+ *
+ * Example:
+ *
+ * Input:  1->2->6->3->4->5->6, val = 6
+ * Output: 1->2->3->4->5
+ */
 public class LC_203 {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode p = dummy;
+        while (p.next != null) {
+            if (p.next.val == val) {
+                p.next = p.next.next;
+            } else
+                p = p.next;
+        }
+        return dummy.next;
+    }
 }
--- src/leetcode/_200_250/_205/LC_205.java
+++ src/leetcode/_200_250/_205/LC_205.java
@@ -1,4 +1,50 @@
 package leetcode._200_250._205;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 205. Isomorphic Strings
+ * Given two strings s and t, determine if they are isomorphic.
+ *
+ * Two strings are isomorphic if the characters in s can be replaced to get t.
+ *
+ * All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character but a character may map to itself.
+ *
+ * Example 1:
+ *
+ * Input: s = "egg", t = "add"
+ * Output: true
+ * Example 2:
+ *
+ * Input: s = "foo", t = "bar"
+ * Output: false
+ * Example 3:
+ *
+ * Input: s = "paper", t = "title"
+ * Output: true
+ */
 public class LC_205 {
+    public boolean isIsomorphic(String s, String t) {
+        if (s == null || t == null) {
+            return false;
+        }
+        Map<Character, Character> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char a = s.charAt(i);
+            char b = t.charAt(i);
+            if (map.containsKey(a)) {
+                if (map.get(a) == b) {
+                    continue;
+                } else
+                    return false;
+            } else {
+                if (!map.containsValue(b)) {
+                    map.put(a, b);
+                } else
+                    return false;
+            }
+        }
+        return true;
+    }
 }
