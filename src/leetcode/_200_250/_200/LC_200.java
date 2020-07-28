aba5647f96a12ecb9fadbf2f5a2d6e9e8f91eaa2
lei zhang <zlandmin@gmail.com>
2020-07-27 21:34:57 -0700

    July 27

--- src/leetcode/_200_250/_200/LC_200.java
+++ src/leetcode/_200_250/_200/LC_200.java
@@ -27,6 +27,32 @@ package leetcode._200_250._200;
  */
 public class LC_200 {
     public int numIslands(char[][] grid) {
-        return 0;
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int result = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == '1') {
+                    result++;
+                    dfs(grid, i, j, rows, cols);
+                }
+            }
+        }
+        return result;
+    }
+
+    private void dfs(char[][] grid, int x, int y, int rows, int cols) {
+        if (x < 0 || x >= rows || y < 0 || y >= cols || grid[x][y] == '0') {
+            return;
+        }
+
+        grid[x][y] = '0';
+        dfs(grid, x + 1, y, rows, cols);
+        dfs(grid, x - 1, y, rows, cols);
+        dfs(grid, x, y + 1, rows, cols);
+        dfs(grid, x, y - 1, rows, cols);
     }
 }
---
+++ src/leetcode/_200_250/_201/LC_201.java
@@ -0,0 +1,29 @@
+package leetcode._200_250._201;
+
+/**
+ * 201. Bitwise AND of Numbers Range
+ * Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, inclusive.
+ *
+ * Example 1:
+ *
+ * Input: [5,7]
+ * Output: 4
+ * Example 2:
+ *
+ * Input: [0,1]
+ * Output: 0
+ */
+public class LC_201 {
+    public int rangeBitwiseAnd(int m, int n) {
+        if (m == 0) {
+            return 0;
+        }
+        int steps = 0;
+        while (m != n) {
+            m >>= 1;
+            n >>= 1;
+            steps++;
+        }
+        return m << steps;
+    }
+}
